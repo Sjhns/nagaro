0e8b56997f00dd94a2398586c5e18ba81624dd07
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-09-22 12:57:05 -0300

    chore: add metadata schema

---
+++ src/types/metadata.d.ts
@@ -0,0 +1,14 @@
+export interface ProfileMetadata {
+  name?: string
+  username?: string
+  display_name?: string
+  picture?: string
+  banner?: string
+  about?: string
+  website?: string
+  lud06?: string
+  lud16?: string
+  nip05?: string
+  created_at?: number
+  npub?: string
+}

bad77269115a6c3005aa3ae6a539f52f64af3f3d
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-09-22 12:39:35 -0300

    feat: add chat and settings page

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,20 @@
+import { Header } from '@/components/header'
+import { Sidebar } from '@/components/sidebar'
+
+export default async function Page() {
+  return (
+    <div className="flex w-full min-h-screen">
+      <div className="flex-1">
+        <Header title="Chat" />
+
+        <div className="flex flex-col items-center justify-center h-full">
+          <p className="text-center text-gray-500 text-2xl font-bold mt-10">
+            Recurso em desenvolvimento.Em breve você poderá conversar com outros
+            usuários da rede nostr. Aguarde!
+          </p>
+        </div>
+      </div>
+      <Sidebar />
+    </div>
+  )
+}
